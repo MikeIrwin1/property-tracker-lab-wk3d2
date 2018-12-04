1e4ffe152f342e6d7125b9a3115a87acd9f1141e
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-12-04 15:18:40 +0000

    save function added

--- console.rb
+++ console.rb
@@ -2,3 +2,4 @@ require('pry')
 require_relative('models/property')
 
 property1 = Property.new({ 'value'=> 10000, 'bedrooms'=> 3, 'year'=> 1900, 'build'=> "flat"})
+property1.save
--- models/property.rb
+++ models/property.rb
@@ -2,20 +2,29 @@ require('pg')
 class Property
 
   attr_accessor :value, :bedrooms, :year, :build
-  attr_reader
+  attr_reader :id
 
 
   def initialize(options)
-  @value = options['value']
-  @bedrooms = options['bedrooms']
-  @year = options['year']
-  @build = options['build']
+    @id = options['id'].to_i if options['id']
+    @value = options['value']
+    @bedrooms = options['bedrooms']
+    @year = options['year']
+    @build = options['build']
 
   end
 
   def save()
     db = PG.connect({dbname: 'properties', host: 'localhost'})
-    sql = "INSERT INTO portfolio (value, bedrooms, year, build)"
+    sql = "INSERT INTO portfolio (value, bedrooms, year, build)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+    "
+    values = [@value, @bedrooms, @year, @build]
+    db.prepare("save", sql)
+    db.exec_prepared("save", values)
+    db.close()
+
   end
 
 
